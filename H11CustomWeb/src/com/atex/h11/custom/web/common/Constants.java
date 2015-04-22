b3f9ae3f0f58e30e9f9422a35ed90539b647edd3
jp <jp.mariano@newscycle.com>
2015-04-23 07:28:21 +0800

    Export XML Servlet updates

--- H11CustomWeb/src/com/atex/h11/custom/web/common/Constants.java
+++ H11CustomWeb/src/com/atex/h11/custom/web/common/Constants.java
@@ -4,7 +4,10 @@ public interface Constants {
 	public static final String JBOSS_HOMEDIR_PROPERTY = "jboss.server.home.dir";
 	public static final String CONFIG_DIR = "conf";
 	
-	public static final String NODETYPE_OBJECT = "object";
+	public static final String NODETYPE_OBJECT = "obj";
 	public static final String NODETYPE_SP = "sp";
-	public static final String NODETYPE_PAGE = "page";
+	public static final String NODETYPE_LOGPAGE = "lp";
+	public static final String NODETYPE_PHYSPAGE = "pp";
+	public static final String NODETYPE_EDITION = "ed";
+	public static final String NODETYPE_NEWSPAPER = "np";
 }
--- H11CustomWeb/src/com/atex/h11/custom/web/export/ExportXMLServlet.java
+++ H11CustomWeb/src/com/atex/h11/custom/web/export/ExportXMLServlet.java
@@ -3,23 +3,27 @@ package com.atex.h11.custom.web.export;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 import java.util.Properties;
-import java.util.Vector;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.atex.h11.custom.common.DataSource;
+import com.atex.h11.custom.common.Edition;
+import com.atex.h11.custom.common.HermesObject;
+import com.atex.h11.custom.common.LogPage;
+import com.atex.h11.custom.common.Newspaper;
+import com.atex.h11.custom.common.PhysPage;
 import com.atex.h11.custom.web.common.Constants;
 import com.unisys.media.cr.adapter.ncm.common.data.pk.NCMObjectPK;
-import com.unisys.media.cr.adapter.ncm.common.data.values.NCMObjectBuildProperties;
 import com.unisys.media.cr.adapter.ncm.model.data.datasource.NCMDataSource;
-import com.unisys.media.cr.adapter.ncm.model.data.values.NCMObjectValueClient;
-import com.unisys.media.extension.common.serialize.xml.XMLSerializeWriter;
+
 
 /**
  * Servlet implementation class ExportServlet
@@ -53,73 +57,132 @@ public class ExportXMLServlet extends HttpServlet {
 	
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
-        ServletOutputStream out = response.getOutputStream();
+        PrintWriter out = response.getWriter();
+        
         NCMDataSource ds = null;
 
         String user = request.getParameter("user");
         String password = request.getParameter("password");
         String sessionId = request.getParameter("sessionid");
         String nodeType = request.getParameter("nodetype");
-        String id = request.getParameter("id");
-        String metadataSchema = request.getParameter("schema");
-        String metadataField = request.getParameter("field");
-        String metadataValue = request.getParameter("value");
 
+        log(request.getParameterMap().toString());  // For debugging only       
+        
         Properties props = getProperties();
+        
+        String convertFormat = props.getProperty("convert.format", "Neutral");
+        String outDir = props.getProperty("output.dir");
+        if (outDir == null || outDir.isEmpty()) {
+        	throw new IllegalArgumentException("Output directory not defined");
+        }
         
         // default credentials
         if (user == null && password == null) {
         	user = props.getProperty("user");
         	password = props.getProperty("password");
         }
-               
-        log(request.getParameterMap().toString());  // For debugging only
-        
+                       
         if (sessionId != null) {
             ds = (NCMDataSource) DataSource.newInstance(sessionId);
         } else {
             ds = (NCMDataSource) DataSource.newInstance(user, password);
         }
                
-        if (nodeType.equalsIgnoreCase(Constants.NODETYPE_OBJECT) || nodeType.equalsIgnoreCase(Constants.NODETYPE_SP)) {
-        	NCMObjectPK objPK = new NCMObjectPK(Integer.parseInt(id));
-            NCMObjectBuildProperties objProps = new NCMObjectBuildProperties();
-            objProps.setIncludeObjContent(true);
-            objProps.setIncludeObjContent(true);
-            objProps.setIncludeLay(true);
-            objProps.setIncludeLayContent(true);
-            objProps.setIncludeLayObjContent(true);
-            objProps.setIncludeAttachments(true);
-            objProps.setIncludeCaption(true);
-            objProps.setIncludeCreditBox(true);
-            objProps.setIncludeIPTC(true);
-            objProps.setIncludeTextPreview(true);
-            objProps.setIncludeLinkedObject(true);
-            objProps.setIncludeVariants(true);
-            objProps.setIncludeSpChild(true);
-            objProps.setXhtmlNestedAsXml(true);
-            objProps.setNeutralNestedAsXml(true);
-            objProps.setIncludeMetadataChild(true);
-            objProps.setIncludeMetadataGroups(new Vector());
-            objProps.setIncludeConvertTo("Neutral");            
-            NCMObjectValueClient objVC = (NCMObjectValueClient) ds.getNode(objPK, objProps);
-            try {
-	            XMLSerializeWriter w = new XMLSerializeWriter(out);
-	            w.writeObject(objVC, objProps);
-	            w.close();            	
-            } catch (Exception e) {
-            	e.printStackTrace();
-            	log(e.toString());
+        try {       	
+            if (nodeType.equalsIgnoreCase(Constants.NODETYPE_OBJECT) || nodeType.equalsIgnoreCase(Constants.NODETYPE_SP)) {
+                String id = request.getParameter("id");
+            	
+            	HermesObject obj = new HermesObject(ds);
+            	obj.setConvertFormat(convertFormat);
+            	
+            	String outFilename;
+            	if (nodeType.equalsIgnoreCase(Constants.NODETYPE_SP)) {
+            		outFilename = "sp_" + id + ".xml";
+            	} else {
+            		outFilename = "obj_" + id + ".xml";
+            	}
+            	File outFile = new File(outDir, outFilename);
+            	
+            	NCMObjectPK pk = new NCMObjectPK(Integer.parseInt(id));
+            	obj.export(pk, outFile);
+            	
+            	showOutputMessage(out, outFile.getPath(), request);
+            	
+            } else if (nodeType.equalsIgnoreCase(Constants.NODETYPE_LOGPAGE)) {
+            	String pubDate = request.getParameter("pubdate");
+            	String edition = request.getParameter("edition");
+            	String level = request.getParameter("level");
+            	String name = request.getParameter("name");
+            	
+                LogPage lp = new LogPage(ds);
+                lp.setConvertFormat(convertFormat);
+                
+            	File outFile = new File(outDir, "logpage_" + name + ".xml");
+            	
+            	lp.write(level, edition, Integer.parseInt(pubDate), name, new FileOutputStream(outFile));
+            	
+            	showOutputMessage(out, outFile.getPath(), request);
+            	
+            } else if (nodeType.equalsIgnoreCase(Constants.NODETYPE_PHYSPAGE)) {
+            	String pubDate = request.getParameter("pubdate");
+            	String level = request.getParameter("level");
+            	String edition = request.getParameter("edition");
+            	String sequenceNum = request.getParameter("number");
+            	
+            	PhysPage pp = new PhysPage(ds);
+            	pp.setConvertFormat(convertFormat);
+            	
+            	File outFile = new File(outDir, "physpage_" 
+            		+ pubDate + "_" + level + "_" + edition + "_" + sequenceNum + ".xml");
+            	
+            	pp.write(level, edition, Integer.parseInt(pubDate), Short.parseShort(sequenceNum), new FileOutputStream(outFile));
+            	
+            	showOutputMessage(out, outFile.getPath(), request);
+            	
+            } else if (nodeType.equalsIgnoreCase(Constants.NODETYPE_EDITION)) {
+            	String pubDate = request.getParameter("pubdate");
+            	String level = request.getParameter("level");
+            	String edition = request.getParameter("edition");
+            	
+            	Edition ed = new Edition(ds);
+            	ed.setConvertFormat(convertFormat);
+            	
+            	File outFile = new File(outDir, "edition_" + pubDate + "_" + level + "_" + edition + ".xml");
+            	
+            	ed.write(level, edition, Integer.parseInt(pubDate), new FileOutputStream(outFile));
+            	
+            	showOutputMessage(out, outFile.getPath(), request);
+            	
+            } else if (nodeType.equalsIgnoreCase(Constants.NODETYPE_NEWSPAPER)) {
+            	String pubDate = request.getParameter("pubdate");
+            	String level = request.getParameter("level");
+            	
+            	Newspaper np = new Newspaper(ds);
+            	np.setConvertFormat(convertFormat);
+            	
+            	File outFile = new File(outDir, "newspaper_" + pubDate + "_" + level + ".xml");
+            	
+            	np.write(level, Integer.parseInt(pubDate), new FileOutputStream(outFile));
+            	
+            	showOutputMessage(out, outFile.getPath(), request);
+            	
+            } else {
+            	throw new IllegalArgumentException("Usupported node type");
+            }        	
+            
+        } catch (Exception e) {
+            log("Exception encountered", e);
+            response.sendError(HttpServletResponse.SC_CONFLICT, e.toString());
+            
+        } finally {
+            out.close();
+            if (ds != null) {
+            	ds.logout();        	
             }
-        	
-        } else if (nodeType.equalsIgnoreCase(Constants.NODETYPE_PAGE)) {
-        	
-        } else {
-        	throw new IllegalArgumentException("Usupported node type");
         }
 	}
 	
-    private Properties getProperties () throws IOException {
+    protected Properties getProperties () throws IOException {
         Properties props = new Properties();
 
         String jbossHomeDir = System.getProperty(Constants.JBOSS_HOMEDIR_PROPERTY);
@@ -135,5 +198,33 @@ public class ExportXMLServlet extends HttpServlet {
 
         return props;
     }			
+    
+    protected void showOutputMessage(PrintWriter out, String outFile, HttpServletRequest request) {
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Export XML</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Parameters:</h1>");
+        
+        Enumeration parameterList = request.getParameterNames();
+        while( parameterList.hasMoreElements() )
+        {
+        	String name = parameterList.nextElement().toString();
+        	String[] values = request.getParameterValues(name);
+            if(values.length > 1) {
+            	for (int i = 0; i < values.length; i++) {
+            		out.println("<p>" + name + "[" + i + "] = " + values[i] + "</p>" );
+            	}
+        	} else {
+        		out.println("<p>" + name + " = " + request.getParameter(name) + "</p>");
+        	}
+        }
+
+        out.println("<h1>Output:</h1>");
+        out.println("<p>Exported to " + outFile + "</p>");
+        out.println("</body>");
+        out.println("</html>");    	
+    }
 
 }
